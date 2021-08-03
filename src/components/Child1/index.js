026190550612c95c6f57ca63a9d37535475f9687
JACK-ZHANG-coming <44993003+JACK-ZHANG-coming@users.noreply.github.com>
2021-08-03 16:26:03 +0800

    父组件调用子组件里面的函数

--- src/components/Child1/index.js
+++ src/components/Child1/index.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 
 const Child1 = (props) => {
-
 
   return (
     <>
       <div style={{ width: '60%', margin: '30px auto', padding: '30px 5px', textAlign: 'center', border: '3px solid yellowgreen' }} >
         <h3>我是子组件1</h3>
         <div>子组件使用父组件的值：{props.parentValue}</div>
-        <button style={{ margin: '20px 0' }} onClick={() => { props.setParentValue('我触发父组件函数了，子组件触发的哟~' + props.parentValue); }}>子组件使用父组件的函数</button>
+        <button style={{ margin: '20px 0' }} onClick={() => { props.setParentValue('我触发父组件函数了，子组件触发的哟~' + props.parentValue); }}>子组件1使用父组件函数</button>
       </div>
     </>
   )
--- src/components/Child2/index.js
+++ src/components/Child2/index.js
@@ -5,14 +5,30 @@ import React, {
   forwardRef
 } from 'react';
 
-const Child2 = (props, ref) => {
-  useImperativeHandle(ref,()=>{
-    return {
-      add:()=>{
 
+const Child2 = (props, ref) => {
+  const [child2Value, setChild2Value] = useState('子组件2数值')
+  useImperativeHandle(ref, () => {
+    return {
+      child2Value, // 对象简写，全写为child2Value: child2Value,
+      areGetChild2Value: () => {
+        return child2Value
+      },
+      areSetChild2Value: () => {
+        setChild2Value(child2Value + '子组件2数值-变了！')
       }
     }
   })
+
+  return (
+    <>
+      <div style={{ width: '60%', margin: '30px auto', padding: '30px 5px', textAlign: 'center', border: '3px solid yellowgreen' }} >
+        <h3>我是子组件2</h3>
+        <div>子组件使用父组件的值：{child2Value}</div>
+        <button style={{ margin: '20px 0' }} onClick={() => { setChild2Value(child2Value + '子组件2数值-变了！'); }}>子组件2内置按钮</button>
+      </div>
+    </>
+  )
 
 }
 
