3e3d97dd782a9dbc12c3b99fa2fc3b7ed043db3e
JACK-ZHANG-coming <44993003+JACK-ZHANG-coming@users.noreply.github.com>
2021-12-01 09:50:14 +0800

    引入相册代码

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Gallery from './components/gallery/gallery'
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Gallery />
-  </React.StrictMode>,
+ReactDOM.render( <
+  React.StrictMode >
+  <
+  App / > {
+    /* <Gallery /> */ } <
+  /React.StrictMode>,
   document.getElementById('root')
 );
-
