11ecebdc4967a4fada07a15e00d1d29495f05a58
zhangqiang <44993003+JACK-ZHANG-coming@users.noreply.github.com>
2022-06-22 23:27:20 +0800

    删除冗余代码；

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Gallery from './components/gallery/gallery'
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Gallery />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
