d4c97e2651a3a3db0c38c7b8ad885de5a1251c6d
zhangqiang <44993003+JACK-ZHANG-coming@users.noreply.github.com>
2022-07-13 11:14:59 +0800

    Update index.jsx

--- src/components/Count/index.jsx
+++ src/components/Count/index.jsx
@@ -1,43 +1,46 @@
-import  React,{Component} from 'react'
+import React, { Component } from 'react'
 import store from '../../redux/store';
 
-export default class Count extends Component{
+export default class Count extends Component {
 
-  state ={
-    count:0
+  constructor(props){
+    super(props);
+    this.state={
+      count:0
+    }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log('组件挂载完毕');
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
     console.log('组件更新完毕');
   }
 
-  increment = ()=>{
+  increment = () => {
     console.log('====');
-    const {value}=this.selectNumber
+    const { value } = this.selectNumber
     // const {count}=this.state
-    store.dispatch({type:'increment',data:value*1})
+    store.dispatch({ type: 'increment', data: value * 1 })
     // this.setState({})
   }
-  decrement = ()=>{
+  decrement = () => {
 
   }
-  incrementIfOdd = ()=>{
+  incrementIfOdd = () => {
 
   }
-  incrementAsync = ()=>{
+  incrementAsync = () => {
 
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <h2>redux计算求和案例-精简版</h2>
         <div>当前求和的值为：{store.getState()}</div>
-        <select ref={c=>this.selectNumber=c}>
+        <select ref={c => this.selectNumber = c}>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
