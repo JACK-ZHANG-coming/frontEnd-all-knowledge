0c414773e2db3afb7570ac78032d3c98e3039fdc
张强 <2422591495@qq.com>
2023-03-30 15:41:59 +0800

    feat: vue3基础知识点总结；

---
+++ examples/vue/a04vue3基础知识/01_src_分析工程结构/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 01_src_分析工程结构 -->
+  <!--vue3的组件模版结构可以没有根标签-->
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ examples/vue/a04vue3基础知识/13_src_toRef和toRefs/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 13_src_toRef和toRefs -->
+  <!--vue3的组件模版结构可以没有根标签-->
+  <button @click="isShowDemo = !isShowDemo">{{ isShowDemo ? '隐藏' : '显示'}}</button>
+  <Demo v-if="isShowDemo"/>
+</template>
+
+<script>
+import Demo from "./components/Demo";
+import { ref } from "vue";
+export default {
+  name: 'App',
+  components: {Demo},
+  setup(){
+    let isShowDemo = ref(true);
+    return {
+      isShowDemo
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
