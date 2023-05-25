c6df7ccf788e7a71f807e6a4af5c2f81f808767a
zhangqiang <44993003+JACK-ZHANG-coming@users.noreply.github.com>
2023-05-25 20:56:04 +0800

    feat: ts泛型；

--- examples/typescript/a02泛型/index.js
+++ examples/typescript/a02泛型/index.js
@@ -1,5 +1,33 @@
-function loggingIdentity(arg) {
-    console.log(arg.length); // Array has a .length, so no more error
-    return arg;
+/**
+ * 场景引入：
+ *
+ */
+/**
+ * 泛型简单使用
+ *
+ */
+/**
+ * 泛型变量
+ *
+ */
+/**
+ * 泛型类型
+ */
+// function loggingIdentity<T>(arg: T[]): T[] {
+//     console.log(arg.length) // Array has a .length, so no more error
+//     return arg
+// }
+// loggingIdentity([1, 2])
+/**
+ * e.g: 定义一个函数，接收3个参数，返回参数为接受的第一个或者第二个参数
+ */
+function returnFirstOrSecond(a, b, flag) {
+    if (flag === void 0) { flag = false; }
+    if (flag) {
+        return a;
+    }
+    else {
+        return b;
+    }
 }
-loggingIdentity([1, 2]);
+console.log(returnFirstOrSecond(111, {}, true));
--- examples/typescript/a02泛型/index.ts
+++ examples/typescript/a02泛型/index.ts
@@ -1,32 +1,37 @@
-
 /**
  * 场景引入：
- * 
+ *
  */
 
 /**
  * 泛型简单使用
- * 
+ *
  */
-
 
 /**
  * 泛型变量
- * 
+ *
  */
-
-
 
 /**
  * 泛型类型
  */
-
+// function loggingIdentity<T>(arg: T[]): T[] {
+//     console.log(arg.length) // Array has a .length, so no more error
+//     return arg
+// }
+// loggingIdentity([1, 2])
 
 /**
- * 泛型类型
+ * e.g: 定义一个函数，接收3个参数，返回参数为接受的第一个或者第二个参数
  */
-function loggingIdentity<T>(arg: T[]): T[] {
-    console.log(arg.length);  // Array has a .length, so no more error
-    return arg;
+function returnFirstOrSecond<T extends string | number, U extends number | object>(a: T, b: U, flag: true | false = false): T | U {
+    if (flag) {
+        return a
+    }
+    else {
+        return b
+    }
 }
-loggingIdentity([1, 2])
+
+console.log(returnFirstOrSecond(111, {}, true));
