11be5dcb262e648bb918bb775d4475abde8e6b96
张强 <2422591495@qq.com>
2023-11-30 10:05:48 +0800

    feat: 增加一个vue3空的测试项目;

---
+++ test/a03jrj_ui_test_project/src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const header = ref('测试组件')
+</script>
+
+<template>
+  <div class="HelloWorld">
+    <div class="header">
+      {{ header }}
+    </div>
+    <div class="content">111</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+$headerHeight: 30px;
+.HelloWorld {
+  height: 100%;
+  width: 100%;
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: $headerHeight;
+    border: 1px solid black;
+  }
+  .content {
+    width: 100%;
+    height: calc(100vh - $headerHeight);
+    border: 1px solid yellowgreen;
+  }
+}
+</style>
