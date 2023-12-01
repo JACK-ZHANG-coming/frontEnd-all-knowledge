ab09e0d7329fd420f3cfcd1fdcded87403d0c0d8
张强 <2422591495@qq.com>
2023-12-01 11:04:07 +0800

    feat: 测试文件引入element plus；

--- test/a03jrj_ui_test_project/src/components/HelloWorld.vue
+++ test/a03jrj_ui_test_project/src/components/HelloWorld.vue
@@ -1,60 +1,60 @@
 <script setup lang="tsx">
 import { ref, reactive } from 'vue'
-import { JTable } from '@jrj-front-end/jrj-ui'
+// import { JTable } from '@jrj-front-end/jrj-ui'
 const header = ref('测试组件')
-const tableConfig = reactive<any>({
-  id: 'deviceAndonTable', //必填唯一
-  align: 'center',
-  height: 770,
-  toolbarConfig: {
-    insert_actived: false,
-    mark_cancel: false,
-    delete: false,
-    save: false
-  },
-  columns: [
-    {
-      field: 'name',
-      title: '设备名称',
-      sortable: true
-    },
-    {
-      field: 'code',
-      title: '编码',
-      sortable: true
-    },
-    {
-      field: 'state',
-      title: '状态',
-      sortable: true
-    },
-    {
-      field: '操作',
-      title: '操作',
-      fixed: 'right',
-      width: 80,
-      isBindStyle: false,
-      slots: {
-        default: () => {
-          return [
-            <>
-              <el-button text>
-                <p class="color_primary">报修</p>
-              </el-button>
-            </>
-          ]
-        }
-      }
-    }
-  ],
-  data: [
-    {
-      name: '全自动模切机',
-      code: 'SMQ162001',
-      state: '正常'
-    }
-  ]
-})
+// const tableConfig = reactive<any>({
+//   id: 'deviceAndonTable', //必填唯一
+//   align: 'center',
+//   height: 770,
+//   toolbarConfig: {
+//     insert_actived: false,
+//     mark_cancel: false,
+//     delete: false,
+//     save: false
+//   },
+//   columns: [
+//     {
+//       field: 'name',
+//       title: '设备名称',
+//       sortable: true
+//     },
+//     {
+//       field: 'code',
+//       title: '编码',
+//       sortable: true
+//     },
+//     {
+//       field: 'state',
+//       title: '状态',
+//       sortable: true
+//     },
+//     {
+//       field: '操作',
+//       title: '操作',
+//       fixed: 'right',
+//       width: 80,
+//       isBindStyle: false,
+//       slots: {
+//         default: () => {
+//           return [
+//             <>
+//               <el-button text>
+//                 <p class="color_primary">报修</p>
+//               </el-button>
+//             </>
+//           ]
+//         }
+//       }
+//     }
+//   ],
+//   data: [
+//     {
+//       name: '全自动模切机',
+//       code: 'SMQ162001',
+//       state: '正常'
+//     }
+//   ]
+// })
 </script>
 
 <template>
@@ -64,7 +64,7 @@ const tableConfig = reactive<any>({
     </div>
     <div class="content">
       <!-- <j-div  :content="123"></j-div> -->
-      <j-table :tableConfig="tableConfig"></j-table>
+      <!-- <j-table :tableConfig="tableConfig"></j-table> -->
     </div>
   </div>
 </template>
