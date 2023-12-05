2d73598d0da2bedf19654ac91e571d15f85c437c
张强 <2422591495@qq.com>
2023-12-05 13:08:46 +0800

    feat: 更新vue3 admin；

---
+++ examples/vue/a11vue3_admin/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <p class="read-the-docs">Vue3 admin</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
