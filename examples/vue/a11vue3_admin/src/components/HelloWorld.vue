a381a8d8dc7a4d21a1ce66a21e84e88bc8ce0c06
张强 <2422591495@qq.com>
2023-12-05 16:18:14 +0800

    feat: vue3 admin配置vite，引入element puls、eslint，配置全局样式；

--- examples/vue/a11vue3_admin/src/components/HelloWorld.vue
+++ examples/vue/a11vue3_admin/src/components/HelloWorld.vue
@@ -1,15 +1,9 @@
 <script setup lang="ts">
-
 defineProps<{ msg: string }>()
-
 </script>
 
 <template>
-  <p class="read-the-docs">Vue3 admin</p>
+  <p>Vue3 admin</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
--- examples/vue/a11vue3_admin/vite.config.ts
+++ examples/vue/a11vue3_admin/vite.config.ts
@@ -1,7 +1,63 @@
+// @ts-ignore
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueSetupExtend from 'vite-plugin-vue-setup-extend-plus'
+// @ts-ignore
+import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueSetupExtend(),
+    ElementPlus({
+      useSource: true
+    })
+  ],
+  base: './',
+  root: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: true,
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id: any) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    },
+    terserOptions: {
+      compress: {
+        //生产环境时移除console
+        drop_console: false,
+        drop_debugger: true
+      }
+    }
+  },
+  //全局变量
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData: `@import "@/assets/css/variable.scss";`
+        additionalData: `@use "@/assets/css/variable.scss" as *;`
+      }
+    }
+  }
 })
