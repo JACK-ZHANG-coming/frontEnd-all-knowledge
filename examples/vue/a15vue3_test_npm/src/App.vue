9de3c483d56e27a24f18057b379f546256ed75c3
张强 <2422591495@qq.com>
2024-06-26 14:30:53 +0800

    feat: 更新模板；

---
+++ examples/vue/a15vue3_test_npm/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="my-app">
+    <h3>组件测试</h3>
+  </div>
+</template>
+
+<style scoped>
+.my-app {
+  width: 100%;
+  height: 100%;
+}
+</style>
