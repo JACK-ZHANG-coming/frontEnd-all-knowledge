d52fd13e97be349f8209f1da25aa740cc669ba97
张强 <2422591495@qq.com>
2024-06-28 14:16:22 +0800

    feat: 增加npm包本地测试；

--- examples/vue/a15vue3_test_npm/src/App.vue
+++ examples/vue/a15vue3_test_npm/src/App.vue
@@ -1,14 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { DemoApi, JrjUtils, demoCom } from 'demo-component/lib/index'
+
+onMounted(() => {
+  JrjUtils.AutoAdaptDpi(true)
+})
+
+const fullScreen = () => {
+  JrjUtils.FullScreen()
+}
+</script>
 
 <template>
   <div class="my-app">
     <h3>组件测试</h3>
+    <div class="content">
+      <el-button @click="fullScreen">点击全屏</el-button>
+    </div>
+    <!-- <demo-com></demo-com> -->
   </div>
 </template>
 
 <style scoped>
 .my-app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
   height: 100%;
+  .content{
+    padding: 20px;
+  }
 }
 </style>
--- examples/vue/a15vue3_test_npm/src/main.ts
+++ examples/vue/a15vue3_test_npm/src/main.ts
@@ -2,4 +2,22 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+
+const app = createApp(App)
+
+// 注册全局公共组件
+const componentList = {}
+for (const [key, component] of Object.entries({ ...ElementPlusIconsVue, ...componentList })) {
+  app.component(key, component)
+}
+
+app.use(ElementPlus, {
+  locale: zhCn,
+  size: 'default'
+})
+app.mount('#app')
